b33c38336448654112c96882433a538a033b1015
bigkangaroo123 <narkarmanas854@gmail.com>
2025-01-07 09:10:01 -0500

    Update 1_insertion.py

--- my_project/pages/1_insertion.py
+++ my_project/pages/1_insertion.py
@@ -1,7 +1,5 @@
 import streamlit as st
 
-st.title("Add a client:")
-
 if 'clients' not in st.session_state:
     st.session_state.clients = [] 
 if 'client_name' not in st.session_state:
@@ -10,6 +8,8 @@ if 'billing_rate' not in st.session_state:
     st.session_state.billing_rate = ''
 
 # ---------- Add client section: -------------
+st.title(" ➕🤝 Add a client:")
+
 client_name = st.text_input("Enter the name of the client:", value="", key="client_name", placeholder="Type client's name here") #value change (try resetting input field)
 billing_rate = st.text_input("Add hourly billing rate with this client", value="", key="billing_rate", placeholder="Type billing rate here") 
 
@@ -21,26 +21,24 @@ if add_client_button:
         st.error("Please input the client's name to proceed...")
     elif not billing_rate:
         st.error("Please also input the billing rate to proceed...")
+    elif not billing_rate.isdigit():
+        st.error("Please enter a valid positive integer for the billing rate.")
     else:
-        try:
-            billing_rate = int(billing_rate)
-            if billing_rate <= 0:
-                st.error("Please enter a positive integer for the billing rate.")
-            else:
-                client =  {
-                    'name' : client_name,
-                    'rate' : billing_rate,
-                    'projects' : []
-                }
-                st.session_state.clients.append(client)
-                st.success(f"Client '{client_name}' with rate ${billing_rate}/hour added!")
-
-        except ValueError:
-            st.error("Please enter a valid integer for the billing rate.")
+        billing_rate = int(billing_rate)
+        if billing_rate <= 0:
+            st.error("Please enter a positive integer for thhe billing rate")
+        else:
+            client = {
+                'name' = client_name
+                'rate' = billing_rate
+                'projects' = []
+            }
+            st.session_state.clients.append(client)
+            st.success(f"Client {client_name} with hourly rate of ${billing_rate} added!")
 
 # ---------- Add project section: -------------
 
-st.title("Add a project:")
+st.title("➕📊 Add a project:")
 
 if st.session_state.clients:
     client_names = []
